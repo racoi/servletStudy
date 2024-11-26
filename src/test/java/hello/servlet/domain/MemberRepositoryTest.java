15c809964e86cad2deb7f79669e4214d0ec403e4
racoi <87438305+racoi@users.noreply.github.com>
2024-11-27 00:55:59 +0900

    jsp

--- src/test/java/hello/servlet/domain/MemberRepositoryTest.java
+++ src/test/java/hello/servlet/domain/MemberRepositoryTest.java
@@ -1,13 +1,13 @@
 package hello.servlet.domain;
 
-import org.assertj.core.api.Assertions;
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 class MemberRepositoryTest {
 
